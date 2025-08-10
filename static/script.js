ca8363696a4e59d48d491fa7ac0622c52f835be6
Priyan Acharya <priyanacharya06@gmail.com>
2025-08-10 13:15:50 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,17 @@
+function startCountdown(seconds) {
+    let timerElement = document.getElementById("timer");
+    let statusElement = document.getElementById("status");
+    let timeLeft = seconds;
+
+    let interval = setInterval(function() {
+        if (timeLeft <= 0) {
+            clearInterval(interval);
+            timerElement.textContent = "Done!";
+            statusElement.textContent = "Enjoy your coffee! ☕";
+        } else {
+            timerElement.textContent = timeLeft;
+        }
+        timeLeft--;
+    }, 1000);
+}
 
